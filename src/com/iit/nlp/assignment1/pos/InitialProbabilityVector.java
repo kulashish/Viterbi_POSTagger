be9d980e731d432f6e0bfbed4d9332628c7d4a6b
Ashish Kulkarni <kulashish@gmail.com>
2012-01-30 14:18:39 +0530

    added initial prob vector

---
+++ src/com/iit/nlp/assignment1/pos/InitialProbabilityVector.java
@@ -0,0 +1,75 @@
+package com.iit.nlp.assignment1.pos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InitialProbabilityVector {
+	private int sum = 0;
+	private List<InitialProbabilityVectorEntry> initialProbList;
+
+	public int getSize() {
+		return null != initialProbList ? initialProbList.size() : 0;
+	}
+
+	public void addState(POSTag tag) {
+		InitialProbabilityVectorEntry foundEntry = null;
+		for (InitialProbabilityVectorEntry entry : getInitialProbList())
+			if (entry.tag.equals(tag)) {
+				foundEntry = entry;
+				break;
+			}
+		if (null == foundEntry) {
+			foundEntry = new InitialProbabilityVectorEntry(tag);
+			initialProbList.add(foundEntry);
+		} else
+			foundEntry.update();
+		sum++;
+	}
+
+	public List<InitialProbabilityVectorEntry> getInitialProbList() {
+		if (null == initialProbList)
+			initialProbList = new ArrayList<InitialProbabilityVectorEntry>();
+		return initialProbList;
+	}
+
+	public void computeProbabilities() {
+		for (InitialProbabilityVectorEntry entry : initialProbList)
+			entry.computeProbability(sum);
+	}
+
+	private class InitialProbabilityVectorEntry {
+		private POSTag tag;
+		private float probability;
+
+		public InitialProbabilityVectorEntry(POSTag tag2) {
+			this.tag = tag2;
+			probability = 1f;
+		}
+
+		public void computeProbability(int sum) {
+			setProbability(probability / sum);
+		}
+
+		public void update() {
+			probability += 1;
+		}
+
+		public POSTag getTag() {
+			return tag;
+		}
+
+		public void setTag(POSTag tag) {
+			this.tag = tag;
+		}
+
+		public float getProbability() {
+			return probability;
+		}
+
+		public void setProbability(float probability) {
+			this.probability = probability;
+		}
+
+	}
+
+}
